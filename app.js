e4f5c34fb07df6b78afe0076075841bc8bf200be
shahd <shahd.jm33@gmail.com>
2021-05-20 00:14:07 +0300

    edit

--- app.js
+++ app.js
@@ -1,100 +1,133 @@
 
+'use strict';
 
 const form = document.getElementById('myForm');
+const clearTable = document.getElementById('clear');
 const tablePlace = document.getElementById('mytable');
-const table = document.createElement('table');
 
-
-imgArray = [
-    'Alstroemerias.jpeg',
-    'Gardenias.jpeg',
-    'Orchids.jpeg',
-    'Peonies.jpeg',
-    'Roses.jpeg',
-    'Sunflowers.jpeg',
-    'Tulips.jpeg'
-];
-console.log(imgArray);
 function flower(name, image, season) {
     this.name = name;
-    this.image = imgArray[i];
+    this.image = image;
     this.season = season
+
     flower.all.push(this);
 }
 
-flower.all = [];
+flower.all = JSON.parse(localStorage.getItem('myFlower'));
+
 form.addEventListener('submit', function (event) {
     event.preventDefault();
     const name = event.target.UserName.value;
-    const image = event.target.userImage.value;
+    const image = event.target.flowers.value;
     const season = event.target.userSeason.value;
+
     new flower(name, image, season);
+
+    localStorage.setItem('myFlower', JSON.stringify(flower.all));
+    flower.all = JSON.parse(localStorage.getItem('myFlower'));
+
+    while (tablePlace.hasChildNodes()) {
+        tablePlace.removeChild(tablePlace.firstChild);
+    }
+
+    renderTableHead();
+    renderTableBody();
+})
+
+clearTable.addEventListener('click', clearList);
+function clearList(event) {
+    event.preventDefault();
+    flower.all= [];
+    
     localStorage.setItem('myFlower', JSON.stringify(flower.all));
 
-})
-console.log(flower.all);
+    while (tablePlace.hasChildNodes()) {
+        tablePlace.removeChild(tablePlace.firstChild);
+    }
+
+    renderTableHead();
+    renderTableBody();
+}
+
 function renderTableHead() {
+
     const thead = document.createElement('thead');
+    thead.setAttribute('class', 'tableHead');
     const trE = document.createElement('tr');
+    const col0 = document.createElement('td');
     const col1 = document.createElement('td');
     const col2 = document.createElement('td');
     const col3 = document.createElement('td');
 
-
+    col0.textContent = "#";
     col1.textContent = "image";
     col2.textContent = "name";
     col3.textContent = "season";
 
-    col1.appendChild(remove);
+    trE.appendChild(col0);
     trE.appendChild(col1);
     trE.appendChild(col2);
     trE.appendChild(col3);
     thead.appendChild(trE);
-    table.appendChild(thead);
-
+    tablePlace.appendChild(thead);
 }
 
-const tbody = document.createElement('tbody');
+renderTableHead();
+renderTableBody();
+
 function renderTableBody() {
-    renderTableBody.innerHTML = '';
+
+    const tbody = document.createElement('tbody');
     for (let i = 0; i < flower.all.length; i++) {
         const trE = document.createElement('tr');
         const col1 = document.createElement('td');
         const col2 = document.createElement('td');
         const col3 = document.createElement('td');
+        const col4 = document.createElement('td');
+
         const remove = document.createElement('button');
-
-        col1.textContent = Event.target.userImage.value;
-        col2.textContent = Event.target.userImage.value;
-        col3.textContent = Event.target.userImage.value;
         remove.textContent = "x";
-
-
+        remove.setAttribute('class', 'removeButton')
+        remove.setAttribute('id', flower.all[i].name)
+        remove.addEventListener('click', removeFlower);
         col1.appendChild(remove);
+
+        var img = document.createElement("img");
+        img.src = "/images/" + flower.all[i].image + ".jpeg";
+        col2.appendChild(img);
+
+        col3.textContent = flower.all[i].name;
+        col4.textContent = flower.all[i].season;
+
         trE.appendChild(col1);
         trE.appendChild(col2);
         trE.appendChild(col3);
-        tbody.appendChild(trE);
-        table.appendChild(tbody);
+        trE.appendChild(col4);
 
-        remove.setAttribute('id', flower.all[i].nam)
-        remove.addEventListener('click', removeFun);
-        function removeFun(event) {
-            event.preventDefault();
-            let id = event.target.id;
-            for (let i = 0; i < flower.all.length; i++) {
-                if (id == flower.all[i].name) {
-                    flower.all.splice(i, 1);
-                }
-            }
-           
-        
-        localStorage.setItem('myFlower'.JSON.stringify(flower.all));
-        table.parentNode.removeChild(table);
-        renderTableHead();
-        renderTableBody();
+        tbody.appendChild(trE);
+        tablePlace.appendChild(tbody);
     }
 }
 
-}
+function removeFlower(event) {
 
+    event.preventDefault();
+
+    let id = event.target.id;
+    for (let i = 0; i < flower.all.length; i++) {
+        if (id == flower.all[i].name) {
+            flower.all.splice(i, 1);
+        }
+    }
+
+    localStorage.setItem('myFlower', JSON.stringify(flower.all));
+    flower.all = JSON.parse(localStorage.getItem('myFlower'));
+    console.log(localStorage.getItem("myFlower"))
+
+    while (tablePlace.hasChildNodes()) {
+        tablePlace.removeChild(tablePlace.firstChild);
+    }
+
+    renderTableHead();
+    renderTableBody();
+}
